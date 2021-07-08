e65cfceba8f5a579c59512aa0f89e1e47eeabc72
Maciej Witowski <witowski.maciej@gmail.com>
2021-07-08 17:54:19 +0200

    Refactor

--- src/main/kotlin/nextbiggernumber.kt
+++ src/main/kotlin/nextbiggernumber.kt
@@ -14,47 +14,15 @@ fun main() {
 }
 
 fun nextBiggerNumber(n: Long): Long {
-  val numbers = n
-      .toString()
-      .map(Character::getNumericValue)
-      .toMutableList()
-
-  var found: Int? = null
-  for (i in numbers.lastIndex downTo 1) {
-    if (numbers[i - 1] < numbers[i]) {
-      found = i - 1
-      break
-    }
-  }
-
-  if (found == null) {
-    return -1
-  }
-
-  val toSwap = numbers
-      .withIndex()
-      .filter { it.index > found && it.value > numbers[found] }
-      .minByOrNull { indexedValue -> indexedValue.value }!!
-
-  val foundBeforeSwap = numbers[found]
-  numbers[found] = toSwap.value
-  numbers[toSwap.index] = foundBeforeSwap
-
-  val new = numbers.slice(0..found) + numbers.slice(found + 1..numbers.lastIndex).sorted()
-
-  return new.joinToString(separator = "").toLong()
-}
-
-fun nextBiggerNumberOther(n: Long): Long {
-  val text = n.toString().toMutableList()
-  for (i in text.size - 2 downTo 0) {
-    if (text[i] < text[i + 1]) {
-      val tail = text.subList(i + 1, text.size)
-      val min = tail.withIndex().filter { it.value > text[i] }.minBy { it.value }!!
-      text[i + 1 + min.index] = text[i]
-      text[i] = min.value
+  val numbers = n.toString().toMutableList()
+  for (i in numbers.lastIndex - 1 downTo 0) {
+    if (numbers[i] < numbers[i + 1]) {
+      val tail = numbers.subList(i + 1, numbers.size)
+      val min = tail.withIndex().filter { it.value > numbers[i] }.minByOrNull { it.value }!!
+      numbers[i + 1 + min.index] = numbers[i]
+      numbers[i] = min.value
       tail.sort()
-      return text.joinToString("").toLong()
+      return numbers.joinToString("").toLong()
     }
   }
   return -1
