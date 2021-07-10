55b46934b5b3ee9352b3ff86861bd51b46861b80
Maciej Witowski <witowski.maciej@gmail.com>
2021-07-10 09:59:22 +0200

    Morse code WIP

--- src/main/kotlin/morsecode.kt
+++ src/main/kotlin/morsecode.kt
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/decode-the-morse-code
 
+import java.lang.IllegalStateException
+import java.lang.UnsupportedOperationException
 import kotlin.test.assertEquals
 
 val MorseCode = mapOf(
@@ -9,11 +11,28 @@ val MorseCode = mapOf(
     ".---" to "J",
     "..-" to "U",
     "-.." to "D",
+    "..." to "T"
 )
 
 fun main() {
-  assertEquals("HEY JUDE", decodeMorse(".... . -.--   .--- ..- -.. ."))
-  assertEquals("E", decodeMorse(" . "))
+  println(decodeBits("111000"))
+}
+
+fun decodeBits(bits: String): String {
+  val trimmed = bits.trim { it == '0' }
+  val unit: Int = TODO()
+  val result = trimmed.split("0".repeat(7 * unit)).joinToString("   ") { word ->
+    word.split("0".repeat(3 * unit)).joinToString(" ") { character ->
+      character.split("0".repeat(unit)).joinToString("") { sign ->
+        when (sign) {
+          "1".repeat(unit) -> "."
+          "1".repeat(3 * unit) -> "-"
+          else -> ""//throw UnsupportedOperationException("Unsupported sign: $sign")
+        }
+      }
+    }
+  }
+  return result
 }
 
 fun decodeMorse(code: String): String =
